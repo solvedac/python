6230d1f05057dc71ba26a91c9d5c99f715341b7b
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-05-12 08:55:30 +0900

    feat: Add `User` class
    - solvedac_community/Models/user.py: Add `User` class
    - solvedac_community/Models/user_tier.py: Add `UserTier` class
    - solvedac_community/Models/class_decoration.py: Add `ClassDecoration` class

---
+++ solvedac_community/Models/user.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2023 DevRuby
+MIT License
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from dataclasses import dataclass
+from typing import Dict, Any, Union
+import datetime
+
+from .user_tier import UserTier
+from .class_decoration import ClassDecoration
+from ..utils import get_datetime_from_string
+
+
+@dataclass
+class User:
+    handle: str
+    bio: str
+    badge_id: Union[str, None]
+    background_id: str
+    profile_image_url: Union[str, None]
+    solved_count: int
+    vote_count: int
+    class_rating: int
+    class_decoration: ClassDecoration
+    rival_count: int
+    reverse_rival_count: int
+    tier: UserTier
+    rank: int
+    exp: int
+    rating_by_problems_sum: int
+    rating_by_class: int
+    rating_by_solved_count: int
+    rating_by_vote_count: int
+    max_streak: int
+    coins: int
+    stardusts: int
+    joined_at: datetime.datetime
+    banned_until: datetime.datetime
+    pro_until: datetime.datetime
+
+    def __init__(self, data: Dict[str, Union[str, int, bool, None]]):
+        self.handle: str = data["handle"]
+        self.bio: str = data["bio"]
+        self.badge_id: Union[str, None] = data.get("badge_id")
+        self.background_id: str = data["backgroundId"]
+        self.profile_image_url: Union[str, None] = data.get("profileImageUrl")
+        self.solved_count: int = data["solvedCount"]
+        self.vote_count: int = data["voteCount"]
+        self.class_rating: int = data["class"]
+        self.class_decoration: ClassDecoration = ClassDecoration(data["classDecoration"])
+        self.rival_count: int = data["rivalCount"]
+        self.reverse_rival_count: int = data["reverseRivalCount"]
+        self.tier: UserTier = UserTier(data["tier"])
+        self.rating: int = data["rating"]
+        self.rank: int = data["rank"]
+        self.exp: int = data["exp"]
+        self.rating_by_problems_sum: int = data["ratingByProblemsSum"]
+        self.rating_by_class: int = data["ratingByClass"]
+        self.rating_by_solved_count: int = data["ratingBySolvedCount"]
+        self.rating_by_vote_count: int = data["ratingByVoteCount"]
+        self.max_streak: int = data["maxStreak"]
+        self.coins: int = data["coins"]
+        self.stardusts: int = data["stardusts"]
+        self.joined_at: datetime.datetime = get_datetime_from_string(data["joinedAt"])
+        self.banned_until: datetime.datetime = get_datetime_from_string(data["bannedUntil"])
+        self.pro_until: datetime.datetime = get_datetime_from_string(data["proUntil"])
