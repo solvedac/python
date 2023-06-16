371168cbc583e072b8958642c29a03685856052f
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-06-16 18:32:08 +0900

    test: Edit account_test unittest code

--- account_test.py
+++ account_test.py
@@ -4,13 +4,17 @@ import unittest
 import os
 
 loop = asyncio.get_event_loop()
-TOKEN = os.environ["SolvedAC"]
-client = SolvedAC.Client(solvedac_token=TOKEN)
+TOKEN1 = os.environ["SolvedAC1"]
+TOKEN2 = os.environ["SolvedAC2"]
+
+client1 = SolvedAC.Client(solvedac_token=TOKEN1)
+client2 = SolvedAC.Client(solvedac_token=TOKEN2)
 
 
 class Test(unittest.TestCase):
     def test_verity_account_credentials(self):
-        print(loop.run_until_complete(client.verify_account_credentials()))
+        print(loop.run_until_complete(client1.verify_account_credentials()))
+        print(loop.run_until_complete(client2.verify_account_credentials()))
 
 
 if __name__ == "__main__":
