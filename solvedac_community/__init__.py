20dbee50ae93ccfc906de394eb7da1fee65d7064
Eunho Lee <hiveruby@gmail.com>
2024-04-11 20:21:42 +0900

    Remove library-checking process

--- solvedac_community/__init__.py
+++ solvedac_community/__init__.py
@@ -11,27 +11,7 @@ An API Wrapper for SolvedAC API
 Copyright (c) 2023 DevRuby
 """
 
+from . import HTTPClients
 from .client import Client
 
-__all__ = ["Client"]
-
-count = 0
-
-try:
-    import aiohttp
-
-    count += 1
-except ImportError:
-    pass
-
-try:
-    import httpx
-
-    count += 1
-except ImportError:
-    pass
-
-if count == 0:
-    raise ImportError(
-        "\nAt least one of aiohttp or httpx libraries is required\nTry `pip install aiohttp` or `pip install httpx`"
-    )
+__all__ = ["Client", "HTTPClients"]
